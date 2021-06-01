c46121878e62020776ad2fb26e07d067fa114031
Nathan L <nathan.lachance@gmail.com>
2021-06-01 16:24:46 -0400

    Added code for the first plot

--- create_plots.R
+++ create_plots.R
@@ -12,3 +12,9 @@ if(!exists("twodays")){
     twodays$Pstd <- strptime(paste(twodays$Date,twodays$Time),"%d/%m/%Y  %H:%M:%S")
     #Pstd stands for pasted data
 }
+
+# create the first graph
+png("plot1.png")
+#Set the color to red, get the titles of each section right, plug in the variable and voila, done
+with(twodays, hist(Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)", col="red"))
+dev.off()
